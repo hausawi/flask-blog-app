03196834221fa5d7c6a63841ff57aa354877e16d
hausawi <haidermoubarak1987@gmail.com>
2025-07-17 17:12:51 +0200

    setup registration functionality to the system

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 from posts import Data
 
@@ -57,9 +57,12 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 
